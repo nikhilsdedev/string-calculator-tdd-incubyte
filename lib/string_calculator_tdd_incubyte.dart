055603933fbdbb84b9bb9e55d396f089de5194ab
Nikhil <nikhil.d@mantralabsglobal.com>
2026-01-17 09:21:25 +0530

    REFACTOR: use map and fold for cleaner sum

--- lib/string_calculator_tdd_incubyte.dart
+++ lib/string_calculator_tdd_incubyte.dart
@@ -4,10 +4,6 @@ class StringCalculator {
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
           var parts = numbers.split(',');
-    var sum = 0;
-    for (var p in parts) {
-      sum += int.parse(p);
-    }
-    return sum;
+   return parts.map((p) => int.parse(p)).fold(0, (a, b) => a + b);
   }
 }
