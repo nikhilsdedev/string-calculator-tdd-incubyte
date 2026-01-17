621bd39da5b3840b0aff45caed667dde31ae5c45
Nikhil <nikhil.d@mantralabsglobal.com>
2026-01-17 08:38:16 +0530

    chore: initialize project structure

---
+++ bin/string_calculator_tdd_incubyte.dart
@@ -0,0 +1,5 @@
+import 'package:string_calculator_tdd_incubyte/string_calculator_tdd_incubyte.dart' as string_calculator_tdd_incubyte;
+
+void main(List<String> arguments) {
+  print('Hello world: ${string_calculator_tdd_incubyte.calculate()}!');
+}
---
+++ test/string_calculator_tdd_incubyte_test.dart
@@ -0,0 +1,8 @@
+import 'package:string_calculator_tdd_incubyte/string_calculator_tdd_incubyte.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
