b20d52245e0548c9139b4a106d5f2cc2b894f633
Nikhil <nikhil.d@mantralabsglobal.com>
2026-01-17 09:34:05 +0530

    GREEN: parse custom delimiter from //[delim] prefix

--- lib/string_calculator_tdd_incubyte.dart
+++ lib/string_calculator_tdd_incubyte.dart
@@ -3,8 +3,17 @@ class StringCalculator {
   /// Adds numbers in the given string and returns the sum.
   int add(String numbers) {
     if (numbers.isEmpty) return 0;
-    var normalized = numbers.replaceAll('\n', ',');
-    var parts = normalized.split(',');
+     var delimiter = ',';
+    var numString = numbers;
+
+    if (numbers.startsWith('//')) {
+      var newlineIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, newlineIndex);
+      numString = numbers.substring(newlineIndex + 1);
+    }
+
+    var normalized = numString.replaceAll('\n', delimiter);
+    var parts = normalized.split(delimiter);
    return parts.map((p) => int.parse(p)).fold(0, (a, b) => a + b);
   }
 }
